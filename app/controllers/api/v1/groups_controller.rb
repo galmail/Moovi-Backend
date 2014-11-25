d5e85447be9b5aa0102067dabceaa388a29bbda4
Gal Dubitski <gal@tocarta.es>
2014-11-25 00:30:50 +0000

    added groups to api

--- app/controllers/api/v1/groups_controller.rb
+++ app/controllers/api/v1/groups_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::GroupsController < Api::BaseController
     
     def index
-      @groups = Group.joins(:users).where(["user_id = ?",current_user.id])
+      @groups = Group.joins(:users).where(["users.id = ?",current_user.id])
     end
     
 
