09bf6c60913028d80518674e1794172d3e4160c3
Gal Dubitski <gal@tocarta.es>
2014-11-30 23:04:38 +0000

    guest clip api

--- app/controllers/api/v1/join_video_controller.rb
+++ app/controllers/api/v1/join_video_controller.rb
@@ -1,7 +1,26 @@
 class Api::V1::JoinVideoController < ApplicationController
     
     def create
-      render :json=> { id: 'aaa' }, status: :created
+      params.require(:url)
+      params.require(:video_id)
+      params.require(:user_id)
+      clip = nil
+      if params[:id]
+        # update clip
+        clip = Clip.find(params[:id])
+        clip.url = params[:url]
+        clip.video_id = params[:video_id]
+        clip.user_id = params[:user_id]
+      else
+        clip_params = params.permit(:url, :message, :video_id, :user_id)
+        clip = Clip.new(clip_params)
+      end
+      
+      if clip.save
+        render :json=> clip.as_json, status: :created
+      else
+        render :json=> clip.errors, status: :unprocessable_entity
+      end
     end
     
 end
