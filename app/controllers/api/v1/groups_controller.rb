2fe9a43ac6e506a1856f48555e4aef513b60226f
Gal Dubitski <gal@tocarta.es>
2014-11-30 15:03:00 +0000

    group api

--- app/controllers/api/v1/groups_controller.rb
+++ app/controllers/api/v1/groups_controller.rb
@@ -4,5 +4,30 @@ class Api::V1::GroupsController < Api::BaseController
       @groups = Group.joins(:users).where(["users.id = ?",current_user.id])
     end
     
+    def create
+      params.require(:video_id)
+      video = Video.find(:video_id)
+      if !video.group.nil?
+        render :json=> video.group.as_json, status: :ok
+      else
+        group = Group.new()
+        group.name = "Group - #{video.title}"
+        if group.save
+          video.group_id = group.id
+          video.save
+          render :json=> group.as_json, status: :created
+        else
+          render :json=> group.errors, status: :unprocessable_entity
+        end
+      end
+    end
+    
+    # def update
+      # params.require(:id)
+      # clip_params = params.permit(:url)
+      # clip = Clip.find(params[:id])
+      # clip.update_attributes(clip_params)
+      # render :json => clip.as_json, status: :ok
+    # end
 
 end
