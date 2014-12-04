f480665fe533043802baf4e44e84556b04a6f6ee
Gal Dubitski <gal@tocarta.es>
2014-12-04 14:06:35 +0000

    fixed video api

--- app/controllers/api/v1/videos_controller.rb
+++ app/controllers/api/v1/videos_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::VideosController < Api::BaseController
     
     def index
       videos_i_made = Video.where(["moderator_id = ?",current_user.id])
-      videos_i_received = Video.where(["receiver_id = ? AND ready = ?",current_user.id,true])
+      videos_i_received = Video.where(["receiver_id = ? AND status = ?",current_user.id,Video.statuses[:ready]])
       #videos_i_participated = Video.joins(:groups,:users).where(["group_id = ?",current_user.id])
       @videos = videos_i_made.concat(videos_i_received)
     end
