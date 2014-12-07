b64d78cdda1c4fe57cb8a660e57e517148d2f9e8
Gal Dubitski <gal@tocarta.es>
2014-12-07 08:23:27 +0000

    video api

--- app/admin/video.rb
+++ app/admin/video.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register Video do
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
-  # permit_params :list, :of, :attributes, :on, :model
+  permit_params :title, :status, :url, :cover, :moderator_id, :receiver_id, :event_id, :group_id
   #
   # or
   #
